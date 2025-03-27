90952f37b7fac7f3621d19a23eabed4974485cf2
Sun Sunny <sol16821@gmail.com>
2025-03-27 23:03:42 +0100

    Move images folder to public and finished Header.vue.

--- hello-vue/src/App.vue
+++ hello-vue/src/App.vue
@@ -1,17 +1,23 @@
-<script>
+<script setup>
 import Header from './components/Header.vue'
 
-  export default {
-    name: 'App',
-  }
 </script>
 
 <template>
   <div id="app">
+    <Header></Header>
+    <MainSection></MainSection>
   </div>
 </template>
 
 <style>
-  #app {
+@import url('https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap');
+
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    max-width: 600px;
+    font-family: 'Inter', sans-serif;
   }
 </style>
--- hello-vue/src/components/Header.vue
+++ hello-vue/src/components/Header.vue
@@ -1,8 +1,31 @@
 <script setup>
+import { ref } from 'vue'
+
+const ihLogoSrc = ref('/images/Ironhack_logo.png')
+const burgerMenu = ref('/images/Burger_menu.png')
 </script>
 
 <template>
+    <header>
+        <img class="logo" :src="ihLogoSrc">
+        <img class="burger" :src="burgerMenu">
+    </header>
 </template>
 
 <style scoped>
+    header {
+        margin: 20px 30px 0;
+        max-width: 540px;
+        width: 100%;
+        background-color: white;
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+
+    }
+
+    .logo {
+        height: 38px;
+        width: 35px;
+    }
 </style>
